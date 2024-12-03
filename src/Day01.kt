e758dbf4a26e1e387a2ed9e811d61c72a57727db
Dr4kn <servicesDrkn@gmail.com>
2024-12-03 17:35:56 +0100

    Day 1 done

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,47 @@
+import kotlin.math.abs
+
 fun main() {
+    fun parse(input: List<String>): List<List<Int>> {
+        return input.map { line ->
+            line.split("   ").map { number ->
+                number.toInt()
+            }
+        }
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val parsed = parse(input)
+
+        val left = parsed.map { line -> line[0] }.sorted()
+        val right = parsed.map { line -> line[1] }.sorted()
+        var sum = 0
+        left.forEachIndexed { index, line ->
+            sum += abs(line - right[index])
+        }
+
+        return sum
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        val parsed = parse(input)
+        val right = HashMap<Int, Int>()
+        parsed.forEach { number ->
+            right[number[1]] = right.getOrDefault(number[1], 0) + 1
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+        }
+        var sum = 0
+        parsed.forEach { number ->
+            if (right[number[0]] != null) {
+                sum += number[0] * right[number[0]]!!
+            }
+        }
+        return sum
+    }
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
