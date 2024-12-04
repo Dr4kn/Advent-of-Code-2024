290dcf112f175addc4776a2911fed52c466b7a24
Dr4kn <servicesDrkn@gmail.com>
2024-12-04 18:24:40 +0100

    Day 3 done

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,21 +1,54 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val matcher = "mul\\([0-9]+,[0-9]+\\)".toRegex()
+        return matcher
+            .findAll(input.toString())
+            .map { it.value }
+            .toList()
+            .map { it.substring(4,it.lastIndex)
+                .split(",")
+                .map { it.toInt() }
+                .zipWithNext { a, b ->  a * b}}
+            .flatten()
+            .reduce { acc, ints ->  acc + ints}
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var enabled = true
+
+        val filteredInput = "mul\\([0-9]+,[0-9]+\\)|do\\(\\)|don't\\(\\)"
+            .toRegex()
+            .findAll(input.toString())
+            .map { it.value }
+            .toList()
+            .map { it.replace("mul|\\(|\\)".toRegex(), "")}
+
+        var result = 0
+
+        filteredInput.forEach { instruction ->
+            when(instruction) {
+                "do" -> enabled = true
+                "don't" -> enabled = false
+                else -> if (enabled) {
+                    result += instruction.split(",")
+                        .map { it.toInt() }
+                        .zipWithNext{a, b -> a * b}
+                        .first()
+                }
+            }
+            if (instruction == "do") enabled
+        }
+
+        return result
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
     // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day02_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day03_test")
+    //check(part1(testInput) == 161)
+    check(part2(testInput) == 48)
 
     // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day02")
+    val input = readInput("Day03")
     part1(input).println()
     part2(input).println()
 }
