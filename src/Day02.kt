9a196337d985f61d68e89f099775f8a8ee401531
Dr4kn <servicesDrkn@gmail.com>
2024-12-04 17:31:57 +0100

    Day 2 done

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,18 +1,53 @@
+import kotlin.math.abs
+
 fun main() {
+    fun parse(input: List<String>): List<List<Int>> {
+        return input.map { line ->
+            line.split(" ")
+                .map { number -> number.toInt() }
+        }
+    }
     fun part1(input: List<String>): Int {
-        return input.size
+        val parsed = parse(input)
+        var result = 0
+        parsed.forEach { line ->
+            val sorted = line.sorted()
+            if (line == line.sorted() || line == line.sortedDescending()) {
+                val adjacentLevels = line
+                    .windowed(2, 1)
+                    .map { comparison -> abs(comparison[0] - comparison[1]) }
+                    .sorted()
+                if (adjacentLevels.first() in 1..3 && adjacentLevels.last() in 1..3) {
+                    result++
+                }
+            }
+        }
+        return result
+    }
+
+    fun isReportSafe(report: MutableList<Int>): Boolean {
+        val levels = report.zipWithNext { a, b ->  b - a}
+        return levels.all { level -> level in 1..3 } || levels.all {  level -> level in -3..-1 }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var result = 0
+        for (report in input) {
+            val numbers = report.split(" ").map { it.toInt() }
+            var isSafe = false
+            for (i in 0..numbers.lastIndex) {
+                isSafe = isReportSafe(numbers.toMutableList().apply { removeAt(i) })
+                if (isSafe) break
+            }
+            if (isSafe) result++
+        }
+        return result
     }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day02_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 2)
+    check(part2(testInput) == 4)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day02")
